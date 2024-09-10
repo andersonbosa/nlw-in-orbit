83e4f70805bb85d7a5fc432dc8e1d1173afc01fe
andersonbosa <andersonbosa0@gmail.com>
2024-09-09 23:14:36 -0300

    add http server

--- src/backend/src/http/server.ts
+++ src/backend/src/http/server.ts
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.listen({ port: 3333, })
+  .then((liveUrl) => {
+    console.log(`HTTP Server Running: "${liveUrl}"`)
+  })
