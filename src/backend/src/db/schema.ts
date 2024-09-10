ecf52bbea10a5c090079bc7843bc0f56d25c94ca
andersonbosa <andersonbosa0@gmail.com>
2024-09-10 00:27:02 -0300

    add paralleldrive/cuid2

--- src/backend/src/db/schema.ts
+++ src/backend/src/db/schema.ts
@@ -1,7 +1,9 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
@@ -10,7 +12,7 @@ export const goals = pgTable('goals', {
 })
 
 export const goalCompletions = pgTable('goal_completions', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => createId()),
   goalId: text('goal_id')
     .references(() => goals.id)
     .notNull(),
