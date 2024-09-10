e6f3b974b92199e8545e32b7816b363d2754b31b
andersonbosa <andersonbosa0@gmail.com>
2024-09-09 23:21:12 -0300

    chore: add biome

--- src/backend/src/http/server.ts
+++ src/backend/src/http/server.ts
@@ -1,8 +1,11 @@
-import fastify from 'fastify'
+import fastify from "fastify";
 
-const app = fastify()
+const app = fastify();
 
-app.listen({ port: 3333, })
+app
+  .listen({
+    port: 3333,
+  })
   .then((liveUrl) => {
     console.log(`HTTP Server Running: "${liveUrl}"`)
   })
