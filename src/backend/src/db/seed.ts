39037b4c5979d7205abe862fda479de7bbb6d232
andersonbosa <andersonbosa0@gmail.com>
2024-09-13 23:21:46 -0300

    feat(seed): Enrich seed data with more goals and completions
    Expands the database seeding process to include additional goals,
    providing a wider range of sample data.
    Also adds more goal completion records, reflecting progress across different goals and days of the week.
    This enhances the development and testing environment with a more realistic and representative dataset.

--- src/backend/src/db/seed.ts
+++ src/backend/src/db/seed.ts
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import { goalsCompletions, goals } from './schema'
 import { dbOrm, dbClient } from './client'
 
-async function seed() {
+async function seed () {
   await dbOrm.delete(goalsCompletions)
   await dbOrm.delete(goals)
 
@@ -13,13 +13,22 @@ async function seed() {
       { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
       { title: 'Correr', desiredWeeklyFrequency: 3 },
       { title: 'Treino pesado', desiredWeeklyFrequency: 2 },
+      { title: 'Beber 2L água', desiredWeeklyFrequency: 5 },
+      { title: 'Meditar', desiredWeeklyFrequency: 3 },
     ])
     .returning()
 
   const startOfWeek = dayjs().startOf('week')
   await dbOrm.insert(goalsCompletions).values([
-    { goalId: addedGoals[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: addedGoals[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    { goalId: addedGoals[0].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    { goalId: addedGoals[0].id, createdAt: startOfWeek.add(2, 'day').toDate() },
+    { goalId: addedGoals[1].id, createdAt: startOfWeek.add(2, 'day').toDate() },
+    { goalId: addedGoals[1].id, createdAt: startOfWeek.add(4, 'day').toDate() },
+    { goalId: addedGoals[2].id, createdAt: startOfWeek.add(4, 'day').toDate() },
+    { goalId: addedGoals[2].id, createdAt: startOfWeek.add(7, 'day').toDate() },
+    { goalId: addedGoals[3].id, createdAt: startOfWeek.add(7, 'day').toDate() },
+    { goalId: addedGoals[4].id, createdAt: startOfWeek.add(10, 'day').toDate() },
+
   ])
 }
 
