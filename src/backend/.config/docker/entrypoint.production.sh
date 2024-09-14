5d11faee4888a0a8fe79b93d76513a9c359f863e
andersonbosa <andersonbosa0@gmail.com>
2024-09-14 15:37:12 -0300

    feat: Introduce local staging environment with Docker and docker-compose
    This commit establishes a dedicated staging environment for the application, leveraging Docker and docker-compose to ensure consistent and reproducible deployments. The staging environment mirrors the production setup closely, enabling thorough testing and validation before releases.
    
    Key aspects of this implementation include:
    
    - Creation of a Dockerfile tailored for the staging environment.
    - Configuration of docker-compose to orchestrate the staging environment, including a PostgreSQL database.
    - Definition of environment variables and health checks specific to the staging setup.
    - Introduction of Makefile commands to streamline interactions with the staging environment.

---
+++ src/backend/.config/docker/entrypoint.production.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+cat <<EOF
+
+|''||''|             .|''''|,        '||            ||
+   ||                ||    ||         ||      ''    ||
+   ||     ||''|,     ||    || '||''|  ||''|,  ||  ''||''
+   ||     ||  ||     ||    ||  ||     ||  ||  ||    ||
+|..||..| .||  ||. ..  |....|' .||.   .||..|' .||.    |..'
+
+Repository: https://github.com/andersonbosa/nlw-in-orbit
+
+ENVIRONMENT: ${NODE_ENV:-production}
+
+EOF
+
+# echo "INFO: Running 'npm install' "
+# npm install
+
+echo "INFO: Running 'npm run start:prod' "
+npm run start:prod
+
+tail -f /dev/null
