b01a9b9a04e37c449f1c7ce38e58cab1e5a0afe0
andersonbosa <andersonbosa0@gmail.com>
2024-09-15 17:50:01 -0300

    fix(home): Display weekly goals summary
    Fetches and displays a summary of the user's weekly goals progress on the home page. This includes the number of completed and total goals, as well as a breakdown of goals completed each day.  An empty state is shown if no goals exist.

--- src/frontend/src/views/home.view.tsx
+++ src/frontend/src/views/home.view.tsx
@@ -1,13 +1,52 @@
+import { useEffect, useState } from 'react'
+
 import { CreateGoalModal } from '../components/create-goal-modal'
 import { EmptyGoals } from '../components/empty-goals'
 import { Summary } from '../components/summary'
 import { Dialog } from '../components/ui/dialog'
+import { env } from '../env'
+
+type GoalsPerDayType = Record<
+  string,
+  {
+    id: string
+    title: string
+    completedAt: string
+  }[]
+>
+
+interface GetWeekSummaryFunctionResponse {
+  summary: {
+    completed: number
+    total: number
+    goalsPerDay: GoalsPerDayType
+  }
+}
 
 export function HomeView() {
+  const [summaryData, setSummaryData] =
+    useState<GetWeekSummaryFunctionResponse | null>(null)
+
+  function setupSummaryData() {
+    fetch(`${env.VITE_BACKEND_API_BASE_URL}/api/summary`)
+      .then(response => response.json())
+      .then(response => response?.data && setSummaryData(response?.data))
+      .catch(console.error)
+
+    return () => {}
+  }
+
+  useEffect(setupSummaryData, [])
+
+  console.log(summaryData)
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {summaryData?.summary && summaryData?.summary?.total > 0 ? (
+        <Summary />
+      ) : (
+        <EmptyGoals />
+      )}
       <CreateGoalModal />
     </Dialog>
   )
