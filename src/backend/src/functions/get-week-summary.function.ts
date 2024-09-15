f6e0fce01f885ce5a0dea76be9ad2fca2438cee8
andersonbosa <andersonbosa0@gmail.com>
2024-09-15 17:39:35 -0300

    feat: Enhance week summary endpoint with detailed goal progress
    The endpoint `/api/summary` now provides a comprehensive weekly summary of goal progress.
    
    The response includes the total completed goals, total goals set for the week, and a breakdown of completed goals for each day. This allows for a more detailed and insightful overview of the user's weekly goal achievement.

--- src/backend/src/functions/get-week-summary.function.ts
+++ src/backend/src/functions/get-week-summary.function.ts
@@ -4,10 +4,26 @@ import { count, gte, lte, and, eq, sql } from 'drizzle-orm'
 import { dbOrm } from '../db/client'
 import { goals, goalsCompletions } from '../db/schema'
 
-export async function getWeekSummaryFunction () {
+type GoalsPerDayType = Record<
+  string,
+  {
+    id: string
+    title: string
+    completedAt: string
+  }[]
+>
+
+interface GetWeekSummaryFunctionResponse {
+  summary: {
+    completed: number
+    total: number
+    goalsPerDay: GoalsPerDayType
+  } | null
+}
+
+export async function getWeekSummaryFunction(): Promise<GetWeekSummaryFunctionResponse> {
   const firstDayOfWeek = dayjs().startOf('week').toDate()
   const lastDayOfWeek = dayjs().endOf('week').toDate()
-
 
   const goalsCreatedUpToWeek = dbOrm.$with('goals_created_up_to_week').as(
     dbOrm
@@ -27,7 +43,9 @@ export async function getWeekSummaryFunction () {
         id: goals.id,
         title: goals.title,
         completedAt: goalsCompletions.createdAt,
-        completedAtDate: sql/* SQL */`DATE(${goalsCompletions.createdAt})`.as('completedAtDate')
+        completedAtDate: sql /* SQL */`DATE(${goalsCompletions.createdAt})`.as(
+          'completedAtDate'
+        ),
       })
       .from(goalsCompletions)
       .innerJoin(goals, eq(goals.id, goalsCompletions.goalId))
@@ -43,7 +61,7 @@ export async function getWeekSummaryFunction () {
     dbOrm
       .select({
         completedAtDate: goalsCompletedInWeek.completedAtDate,
-        completions: sql/* SQL */`
+        completions: sql /* SQL */`
           JSON_AGG(
             JSON_BUILD_OBJECT(
               'id', ${goalsCompletedInWeek.id},
@@ -51,7 +69,7 @@ export async function getWeekSummaryFunction () {
               'completedAt', ${goalsCompletedInWeek.completedAt}
             )
           )
-        `.as('completions')
+        `.as('completions'),
       })
       .from(goalsCompletedInWeek)
       .groupBy(goalsCompletedInWeek.completedAtDate)
@@ -60,18 +78,24 @@ export async function getWeekSummaryFunction () {
   const result = await dbOrm
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeedDay)
     .select({
-      completed: sql/* SQL */` (SELECT COUNT(*) FROM ${goalsCompletedInWeek}) `.mapWith(Number),
-      total: sql/* SQL */` (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek}) `.mapWith(Number),
-      goalsPerDay: sql/* SQL */`
+      completed:
+        sql /* SQL */` (SELECT COUNT(*) FROM ${goalsCompletedInWeek}) `.mapWith(
+          Number
+        ),
+      total:
+        sql /* SQL */` (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek}) `.mapWith(
+          Number
+        ),
+      goalsPerDay: sql /* SQL */<GoalsPerDayType>`
         JSON_OBJECT_AGG(
           ${goalsCompletedByWeedDay.completedAtDate},
           ${goalsCompletedByWeedDay.completions}
         )
-      `
+      `,
     })
     .from(goalsCompletedByWeedDay)
 
   return {
-    summary: result
+    summary: result.length ? result[0] : null,
   }
 }
--- src/backend/src/routes/get-week-summary.route.ts
+++ src/backend/src/routes/get-week-summary.route.ts
@@ -1,14 +1,11 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { getWeekSummaryFunction } from '../functions/get-week-summary.function'
 
-export const getWeekSummaryRoute: FastifyPluginAsyncZod = async (app) => {
-  app.get(
-    '/api/summary',
-    async () => {
-      const { summary } = await getWeekSummaryFunction()
-      return {
-        data: summary
-      }
+export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/api/summary', async () => {
+    const { summary } = await getWeekSummaryFunction()
+    return {
+      data: { summary },
     }
-  )
+  })
 }
