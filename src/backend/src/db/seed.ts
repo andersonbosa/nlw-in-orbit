33d20147c5ff709ddc87a57046db3b02100f7ff9
andersonbosa <andersonbosa0@gmail.com>
2024-09-17 19:48:27 -0300

    feat(seed): randomize created at timestamp for goal completions

--- src/backend/src/db/seed.ts
+++ src/backend/src/db/seed.ts
@@ -1,7 +1,16 @@
 import dayjs from 'dayjs'
+import { ptBR } from 'dayjs/locale/pt-br'
+dayjs.locale(ptBR)
+
+import { faker } from '@faker-js/faker'
+faker.locale = 'pt_BR'
 
 import { goalsCompletions, goals } from './schema'
 import { dbOrm, dbClient } from './client'
+
+const getRandomMinutes = (): number => {
+  return faker.number.int({ min: 10, max: 1000 })
+}
 
 async function seed() {
   await dbOrm.delete(goalsCompletions)
@@ -20,11 +29,41 @@ async function seed() {
 
   const startOfWeek = dayjs().startOf('week')
   await dbOrm.insert(goalsCompletions).values([
-    { goalId: addedGoals[0].id, createdAt: startOfWeek.add(0, 'day').toDate() },
-    { goalId: addedGoals[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
-    { goalId: addedGoals[2].id, createdAt: startOfWeek.add(2, 'day').toDate() },
-    { goalId: addedGoals[3].id, createdAt: startOfWeek.add(3, 'day').toDate() },
-    { goalId: addedGoals[4].id, createdAt: startOfWeek.add(4, 'day').toDate() },
+    {
+      goalId: addedGoals[0].id,
+      createdAt: startOfWeek
+        .add(0, 'day')
+        .add(getRandomMinutes(), 'minutes')
+        .toDate(),
+    },
+    {
+      goalId: addedGoals[1].id,
+      createdAt: startOfWeek
+        .add(1, 'day')
+        .add(getRandomMinutes(), 'minutes')
+        .toDate(),
+    },
+    {
+      goalId: addedGoals[2].id,
+      createdAt: startOfWeek
+        .add(2, 'day')
+        .add(getRandomMinutes(), 'minutes')
+        .toDate(),
+    },
+    {
+      goalId: addedGoals[3].id,
+      createdAt: startOfWeek
+        .add(3, 'day')
+        .add(getRandomMinutes(), 'minutes')
+        .toDate(),
+    },
+    {
+      goalId: addedGoals[4].id,
+      createdAt: startOfWeek
+        .add(4, 'day')
+        .add(getRandomMinutes(), 'minutes')
+        .toDate(),
+    },
   ])
 }
 
